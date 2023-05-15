da5373ae6e00a73b0362d6101fb9cb91b8e4b4cd
farrelval <farrelawewra@gmail.com>
2023-05-15 22:27:25 +0700

    update bookmark

--- app.js
+++ app.js
@@ -14,5 +14,27 @@ $(".logo").on("click", function(){
     $(".sidebar").removeClass("active")
 });
 
+const apply = document.querySelector(".btn-apply");
 
+function ubahOpacityApply(){
+    apply.style.opacity = "50%"
+}
+
+const save = document.querySelector(".btn-save");
+
+function ubahOpacitySave(){
+    save.style.opacity = "50%"
+}
+
+const applyed = document.querySelector(".btn-applyed");
+
+function ubahOpacityApplyed(){
+    applyed.style.opacity = "100%"
+}
+
+const saved = document.querySelector(".btn-saved");
+
+function ubahOpacitySaved(){
+    saved.style.opacity = "100%"
+}
 
