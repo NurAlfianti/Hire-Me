5af5dbe08ea1d0afd3788172b2ff628df59d56ec
farrelval <farrelawewra@gmail.com>
2023-05-16 23:19:59 +0700

    rapihin code

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-$(".card").on("click", function(){
+$(".card").on("click", function () {
     $(".detail").addClass("active");
 });
 
-$(".close-detail").on("click", function(){
+$(".close-detail").on("click", function () {
     $(".detail").removeClass("active")
 });
 
-$(".menu-bar").on("click", function(){
+$(".menu-bar").on("click", function () {
     $(".sidebar").addClass("active")
 })
 
-$(".logo").on("click", function(){
+$(".logo").on("click", function () {
     $(".sidebar").removeClass("active")
 });
 
 const apply = document.querySelector(".btn-apply");
 
-function ubahOpacityApply(){
+function ubahOpacityApply() {
     apply.style.opacity = "50%"
     alert("You Have Been Apply to this Job");
 }
 
 const save = document.querySelector(".btn-save");
 
-function ubahOpacitySave(){
+function ubahOpacitySave() {
     save.style.opacity = "50%"
 }
 
 const applyed = document.querySelector(".btn-applyed");
 
-function ubahOpacityApplyed(){
+function ubahOpacityApplyed() {
     applyed.style.opacity = "100%"
 }
 
 const saved = document.querySelector(".btn-saved");
 
-function ubahOpacitySaved(){
+function ubahOpacitySaved() {
     saved.style.opacity = "100%"
 }
 
 const filterButton = document.querySelector("#filter").children;
 const item = document.querySelector(".wrapper").children;
 
-for (let i=0; i < filterButton.length; i++){
-    filterButton[i].addEventListener("click", function(){
-        for (let j = 0; j <filterButton.length; j++){
+for (let i = 0; i < filterButton.length; i++) {
+    filterButton[i].addEventListener("click", function () {
+        for (let j = 0; j < filterButton.length; j++) {
             filterButton[j].classList.remove("active")
         }
         this.classList.add("active");
@@ -52,7 +52,7 @@ for (let i=0; i < filterButton.length; i++){
 
         for (let k = 0; k < item.length; k++) {
             item[k].style.display = "none";
-            if (target == item[k].getAttribute("data-id")){
+            if (target == item[k].getAttribute("data-id")) {
                 item[k].style.display = "flex";
             }
             if (target == "All") {
